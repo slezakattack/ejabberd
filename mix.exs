87987bc1c6c44ecfb22670c570c85884d2eda319
Michael Slezak <michael_slezak08@hotmail.com>
2024-08-16 09:13:08 -0600

    test

--- mix.exs
+++ mix.exs
@@ -43,10 +43,11 @@ defmodule Ejabberd.MixProject do
 
   def application do
     [mod: {:ejabberd_app, []},
-     extra_applications: [:idna, :inets, :kernel, :sasl, :ssl, :stdlib, :mix,
-                    :fast_tls, :fast_xml, :fast_yaml, :jose, :logger,
+     applications: [:idna, :inets, :kernel, :sasl, :ssl, :stdlib, :mix,
+                    :fast_tls, :fast_xml, :fast_yaml, :jose,
                     :p1_utils, :stringprep, :syntax_tools, :yconf, :xmpp]
      ++ cond_apps(),
+     extra_applications: [:logger],
      included_applications: [:mnesia, :os_mon,
                              :cache_tab, :eimp, :mqtree, :p1_acme,
                              :p1_oauth2, :pkix]
